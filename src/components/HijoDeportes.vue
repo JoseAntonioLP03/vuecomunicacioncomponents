393228a24afcc56403ef42f23d97552693a8a8ae
JoseAntonioLP03 <l.p.joseantonio03@gmail.com>
2025-10-27 14:16:10 +0100

    first commit

---
+++ src/components/HijoDeportes.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h4 style="color: red;">{{ nombredeporte }}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"HijoDeportes",
+    props:["nombredeporte"]
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h4 style="color: blue;">Padre Deportes</h4>
+        <ul>
+            <li v-for="deporte in deportes" :key="deporte">
+                <HijoDeportes :nombredeporte="deporte"/>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import HijoDeportes from './HijoDeportes.vue';
+export default {
+    name:"PadreDoportes",
+    components:{
+        HijoDeportes
+    },
+    data(){
+        return{
+            deportes:["Futbol","Basquetball","Tennis","Rugby"]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
